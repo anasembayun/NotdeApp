fadcdb0ea92abce8e1e8f32755da8ce93e309dfb
nancylaylaa <nancylaylaa@gmail.com>
2022-06-23 19:29:08 +0700

    widget java class

---
+++ app/src/main/java/com/example/notdeapp/NotdeWidget.java
@@ -0,0 +1,41 @@
+package com.example.notdeapp;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import com.example.notdeapp.Activity.CreateActivity;
+import com.example.notdeapp.Activity.MainActivity;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class NotdeWidget extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            Intent intent = new Intent(context, CreateActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.notde_widget);
+            views.setOnClickPendingIntent(R.id.notde_widget_button, pendingIntent);
+
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
